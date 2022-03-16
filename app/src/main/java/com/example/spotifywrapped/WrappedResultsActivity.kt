bebd8fe96368855722105ca2be111dd543fc87e1
hannahmaung <hannahmaung33@gmail.com>
2022-03-16 16:18:38 -0700

    Added "Share" implicit intent

--- app/src/main/java/com/example/spotifywrapped/WrappedResultsActivity.kt
+++ app/src/main/java/com/example/spotifywrapped/WrappedResultsActivity.kt
@@ -1,11 +1,44 @@
 package com.example.spotifywrapped
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class WrappedResultsActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wrapped_results)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.share_results,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_share -> {
+                shareResults()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun shareResults() {
+
+        val text = getString(R.string.share_text)
+        val intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, text)
+            type = "text/plain"
+        }
+        startActivity(Intent.createChooser(intent, null))
+
     }
 }
